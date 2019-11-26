3a2c2d2dc027969d4def07191f44164e899c807f
hsagehorn18 <50709396+hsagehorn18@users.noreply.github.com>
2019-11-26 10:07:26 -0600

    Add files via upload

---
+++ PigLatinConsoleProject/PigLatinConsoleProject/Program.cs
@@ -0,0 +1,127 @@
+﻿// Pig Latin Translator
+// By: Henry Sagehorn
+// This program takes a word or sentence, and translates it into pig latin. It will loop through until
+// user decides to exit.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigLatinConsoleProject
+{
+    public class PigLatin
+    {
+        List<string> sentenceWordList = new List<string>();
+
+        public string PigLatinTranslator(string words)
+        {
+            const string vowels = "AEIOUaeio";
+            string translation = "";
+            string finalTranslation = "";
+            SentenceReader(words);
+            foreach(string wordPart in sentenceWordList) { 
+            string word = wordPart.ToUpper();
+            int stringLength = word.Length;
+            char firstLetters = word[0];
+                char secondLetter = ' ';
+            if (stringLength > 1)
+                {
+                   secondLetter = word[1];
+                }
+                
+                
+                int letter = vowels.IndexOf(firstLetters);
+                int letterTwo = vowels.IndexOf(secondLetter);
+
+            if (letter == -1) 
+                {
+                if (letterTwo == -1) 
+                    {
+                    word = word.ToLower();
+                    firstLetters = word[0];
+                    secondLetter = word[1];
+                        if (secondLetter == 'y' && word.Length < 3)
+                        {
+                            translation = secondLetter + "-" + firstLetters + "ay";
+                        }
+                        else if(secondLetter == 'y' && word.Length > 2){
+                            string temp = word.Substring(1);
+                            translation = temp + "-" + firstLetters + "ay";
+                        }
+                        else
+                        {
+                            string temp = word.Substring(2);
+                            translation = temp + "-" + firstLetters + secondLetter + "ay";
+                        }
+                    
+                }
+                else
+                {
+                    word = word.ToLower();
+                    firstLetters = word[0];
+                    string temp = word.Substring(1);
+                    translation = temp + "-" + firstLetters + "ay";
+                }
+            }
+            else
+            {
+                word = word.ToLower();               
+                translation = word + "-" + "ay";
+            }
+                
+                finalTranslation = finalTranslation + translation + ' ';
+            }
+            return finalTranslation;
+        }
+
+        public string SentenceReader(string sentence)
+        {
+           string[] wordList = sentence.Split(' ');
+            foreach(var word in wordList)
+            {
+                sentenceWordList.Add(word);
+            }
+            return null;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PigLatin PigLatin = new PigLatin();
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write("Enter a word or sentence to translate into Pig Latin: ");
+            string entry = Console.ReadLine();
+                        
+
+            string translation = PigLatin.PigLatinTranslator(entry);
+
+            //Console.Write(entry);
+            //Console.Write(" in Pig Latin is: ");
+            Console.WriteLine(translation);
+                Console.WriteLine("ometimes-say i-ay it-say uietly-qay at-ay ight-nay and-ay ink-thay about-ay ere-whay y-may ife-lay as-hay one-gay ");
+
+            Console.Write("Would you like to translate another word? Enter 'Y' to continue or any key to exit : ");
+            string choice = Console.ReadLine();
+                Console.WriteLine();
+            choice = choice.ToUpper();
+            if (choice == "Y")
+            {
+                    continue;
+            }
+            else
+            {
+                    flag = false;
+            }
+            
+                
+            }
+        }
+    }
+}
---
+++ PigLatinConsoleProject/PigLatinUnitTests/UnitTest1.cs
@@ -0,0 +1,140 @@
+﻿// Unit tests used to test the program
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PigLatinConsoleProject;
+
+namespace PigLatinUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BasicPigLatinTest()
+        {
+            PigLatin PigLatin = new PigLatin();
+            string word = "hello";
+            string actual = "";
+            string expected = "ello-hay ";
+
+            actual = PigLatin.PigLatinTranslator(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BasicPigLatinTest2()
+        {
+            PigLatin PigLatin = new PigLatin();
+            string word = "switch";
+            string actual = "";
+            string expected = "itch-sway ";
+
+            actual = PigLatin.PigLatinTranslator(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void BasicPigLatinTest3()
+        {
+            PigLatin PigLatin = new PigLatin();
+            string word = "banana";
+            string actual = "";
+            string expected = "anana-bay ";
+
+            actual = PigLatin.PigLatinTranslator(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void BasicPigLatinTest4()
+        {
+            PigLatin PigLatin = new PigLatin();
+            string word = "climb";
+            string actual = "";
+            string expected = "imb-clay ";
+
+            actual = PigLatin.PigLatinTranslator(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BasicPigLatinTest5()
+        {
+            PigLatin PigLatin = new PigLatin();
+            string word = "it";
+            string actual = "";
+            string expected = "it-ay ";
+
+            actual = PigLatin.PigLatinTranslator(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BasicPigLatinTest6Sentence()
+        {
+            PigLatin PigLatin = new PigLatin();
+            string word = "The quick brown fox jumps over the fence";
+            string actual = "";
+            string expected = "e-thay uick-qay own-bray ox-fay umps-jay over-ay e-thay ence-fay ";
+
+            actual = PigLatin.PigLatinTranslator(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BasicPigLatinTest7Sentence()
+        {
+            PigLatin PigLatin = new PigLatin();
+            string word = "Mother wants her good silver back tomorrow";
+            string actual = "";
+            string expected = "other-may ants-way er-hay ood-gay ilver-say ack-bay omorrow-tay ";
+
+            actual = PigLatin.PigLatinTranslator(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BasicPigLatinTest8Sentence()
+        {
+            PigLatin PigLatin = new PigLatin();
+            string word = "Sometimes I sit quietly at night and think about where my life has gone";
+            string actual = "";
+            string expected = "ometimes-say i-ay it-say uietly-qay at-ay ight-nay and-ay ink-thay about-ay ere-whay y-may ife-lay as-hay one-gay ";
+
+            actual = PigLatin.PigLatinTranslator(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BasicPigLatinTest9Sentence()
+        {
+            PigLatin PigLatin = new PigLatin();
+            string word = "This test sentence contains the words zebra and xylophone";
+            string actual = "";
+            string expected = "is-thay est-tay entence-say ontains-cay e-thay ords-way ebra-zay and-ay ylophone-xay ";
+
+            actual = PigLatin.PigLatinTranslator(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BasicPigLatinTest10()
+        {
+            PigLatin PigLatin = new PigLatin();
+            string word = "why";
+            string actual = "";
+            string expected = "y-whay ";
+
+            actual = PigLatin.PigLatinTranslator(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
